7390fbd2e4e8524f6cb3598490c138e50c6b80b4
Neel Maddali <neel@Neels-MacBook-Air.local>
2024-11-14 22:27:16 -0600

    Add volume_of_sphere.py

---
+++ volume_of_sphere.py
@@ -0,0 +1,5 @@
+def calculate_volume_of_sphere(radius):
+    volume_of_sphere = radius**3*4/3*3.14
+    return(volume_of_sphere)
+print(calculate_volume_of_sphere(30))
+print(calculate_volume_of_sphere(40))
